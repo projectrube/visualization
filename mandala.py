2bb176054feccb9769dbd85929a6ddc8e7e46966
Kevin Bluer <kevin@bluer.com>
2022-12-20 13:15:41 -0600

    examples

---
+++ mandala.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+tor = Turtle()
+
+for times in range(36):
+    tor.color("blue")
+    tor.speed(11)
+    tor.circle(100)
+    tor.color("red")
+    tor.forward(200)
+    tor.left(120)
+    tor.color("orange")
+    tor.forward(100)
+    tor.right(120)
+    tor.left(170)
+    tor.left(20)
+    tor.forward(15)
+    
